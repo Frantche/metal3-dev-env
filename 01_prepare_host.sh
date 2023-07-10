03690e54ddeb7b6acda865ea5ce1d64f8845d51f
KLIEBER Francois Ext INNOV/IT-S <HMZS5721@orange.com>
2023-07-10 11:10:33 +0200

    tests

--- 01_prepare_host.sh
+++ 01_prepare_host.sh
@@ -72,10 +72,10 @@ source lib/network.sh
 
 # TODO: since ansible 8.0.0, pinning by digest is PITA, due additional ansible
 # dependencies, which would need to be pinned as well, so it is skipped for now
-if [ -z ${HTTPS_PROXY+x} ]; then 
-  sudo python -m pip install ansible=="${ANSIBLE_VERSION}" --proxy=="${HTTPS_PROXY}"
+if [ -z ${HTTPS_PROXY} ]; then 
+  sudo python -m pip install ansible=="${ANSIBLE_VERSION}"
 else
-  sudo python -m pip install ansible=="${ANSIBLE_VERSION}";
+  sudo python -m pip install ansible=="${ANSIBLE_VERSION}" --proxy=="${HTTPS_PROXY}"
 fi
 
 
